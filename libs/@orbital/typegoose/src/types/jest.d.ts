5ddddf767f72c795e69a7981506f39a58f65a649
Artur Moczulski <artur.moczulski@gmail.com>
2025-06-29 03:21:37 -0700

    build(deps): update @types/jest to v30.0.0 and remove custom type definitions
    The update to @types/jest v30.0.0 allows removal of the custom Jest type definitions
    that were previously needed. This change affects the typegoose package.

--- libs/@orbital/typegoose/src/types/jest.d.ts
+++
@@ -1,7 +0,0 @@
-// Type definitions for Jest
-declare const describe: (name: string, fn: () => void) => void;
-declare const it: (name: string, fn: () => void | Promise<void>) => void;
-declare const expect: any;
-declare const beforeEach: (fn: () => void | Promise<void>) => void;
-declare const afterEach: (fn: () => void | Promise<void>) => void;
-declare const jest: any;
